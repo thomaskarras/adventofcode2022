e1cff6d24aab513ba6797142a6a806abbbdaa18b
Thomas Karras <karras.thomas@gmail.com>
2022-12-05 16:39:31 -0500

    day4 complete

---
+++ day4/day4.clj
@@ -0,0 +1,62 @@
+(ns day4)
+
+
+(def input (-> 
+            (slurp "e:/projects/adventofcode2022/day4/input.txt")
+            (clojure.string/split-lines)))
+
+
+(def sample (take 5 input))
+
+; "28-47" -> [28 47]
+(defn parse-single [x]
+  (->> (clojure.string/split x #"-")
+       (map #(Integer/parseInt %)))) 
+
+; "28-47,45-47" -> [[28 47] [45 47]] 
+(defn parse [x]
+  (->> (clojure.string/split x #",")
+       (map parse-single)))
+
+(defn compare-pair [pair]
+  (let [x (first pair)
+        x1 (first x)
+        x2 (second x)
+        y (second pair)
+        y1 (first y)
+        y2 (second y)]
+    (cond 
+      (and (>= x1 y1) (<= x2 y2)) 1
+      (and (>= y1 x1) (<= y2 x2)) 1
+      :else 0
+      )
+    )
+  )
+
+(defn compare-overlap-pair [pair]
+  (let [x (first pair)
+        x1 (first x)
+        x2 (second x)
+        y (second pair)
+        y1 (first y)
+        y2 (second y)]
+    (cond
+      (or (and (>= x1 y1) (<= x1 y2)) (and (>= x2 y1) (<= x2 y2))) 1
+      (or (and (>= y1 x1) (<= y1 x2)) (and (>= y2 x1) (<= y2 x2))) 1
+      :else 0)))
+
+; part 1
+(->> (map parse input)
+     (map compare-pair)
+     (reduce (fn[total x] (+ total x))))
+
+; part 2
+(->> (map parse input)
+     (map compare-overlap-pair)
+     (reduce (fn [total x] (+ total x))))
+
+
+
+(comment 
+  sample 
+  )
