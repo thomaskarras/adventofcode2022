78f242761276ae5d34e9ff47e20f83d9fc1669f6
Thomas Karras <karras.thomas@gmail.com>
2022-12-05 12:40:20 -0500

    day 1 complete

---
+++ day1/day1.clj
@@ -0,0 +1,26 @@
+(ns day1)
+
+
+(let [input (-> (slurp "e:/projects/adventofcode2022/day1/input.txt")
+                (clojure.string/split-lines))
+      totals 
+      (->> 
+       (partition-by empty? input)
+       (filter #(when (seq (first %)) %))
+       (map (fn [x] (map #(Integer/parseInt %) x) ))
+       (map (fn [x] (reduce (fn [all, val] (+ all val)) x)))
+       )]
+  (def all-totals totals)
+  (def top (apply max totals))
+  (def top3 (->> totals
+                 (sort >)
+                 (take 3))) 
+  (def top3Totals (reduce (fn [total x] (+ total x)) top3)))
+
+
+(comment
+  (-> (java.io.File. ".") .getAbsolutePath)
+  all-totals
+  top
+  top3
+  top3Totals)
